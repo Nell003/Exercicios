1546e282f77c552dc73cac5875bfe7b1133fdf22
Nell003 <cronax003@hotmail.com>
2023-02-23 19:51:43 -0300

    Pares

---
+++ Impares/src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class App {
+
+public static void main(String[] args) {
+   
+
+    
+
+
+    for (int i = 1; i <= 50; i++){
+        if (i % 2 == 0) {
+            System.out.println("Os números pares entre 1 e 50 são: " + i);
+        }
+
+        
+    }
+
+    
+}
+
+
+
+}
+
