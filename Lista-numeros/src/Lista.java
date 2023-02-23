4466dcf8d604b52533b8d229e47584c74af682cc
Nell003 <cronax003@hotmail.com>
2023-02-23 19:02:15 -0300

    Lista

---
+++ Lista-numeros/src/Lista.java
@@ -0,0 +1,18 @@
+public class Lista {
+
+    public static void main(String[] args) {
+        System.out.println();
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.print(i + " ");
+        }
+
+        
+
+    }
+  
+}
