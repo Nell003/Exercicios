97459527fd036bccc33e55b53188dcea7091e368
Nell003 <cronax003@hotmail.com>
2023-03-01 03:37:11 -0300

    random-array

---
+++ num-aleatorios-array/src/App.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+
+
+
+            }
+
+            System.out.println("Numeros aleatorios: ");
+           for (int numero : numerosAleatorios ) {
+
+                System.out.print(numero + " ");
+            
+           } 
+           
+           
+}
+}
